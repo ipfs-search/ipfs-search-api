049c6676193093d7c9812a81aa82813a2322eca0
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2018-11-28 23:33:33 +0000

    Prefer newer results!

--- search.js
+++ search.js
@@ -11,28 +11,166 @@ var client = new elasticsearch.Client({
 
 function search(q, page, page_size) {
   var body = {
-      "query": {
+    "query": {
+      "function_score": {
+        "query": {
           "query_string": {
-              "query": q,
-              "default_operator": "AND"
+            "query": q,
+            "default_operator": "AND"
           }
-      },
-      "highlight": {
-          "order" : "score",
-          "require_field_match": false,
-          "encoder": "html",
-          "fields": {
-              "*": {
-                  "number_of_fragments" : 1,
-                  "fragment_size" : result_description_length
+        },
+        "score_mode": "sum",
+        "boost_mode": "multiply",
+        "functions": [
+          {
+            "weight": 1
+          },
+          {
+            "filter": {
+              "range": {
+                "last-seen": {
+                  "from": "now-3M"
+                }
               }
+            },
+            "weight": 1
+          },
+          {
+            "filter": {
+              "range": {
+                "last-seen": {
+                  "from": "now-1M"
+                }
+              }
+            },
+            "weight": 1
+          },
+          {
+            "filter": {
+              "range": {
+                "last-seen": {
+                  "from": "now-1d"
+                }
+              }
+            },
+            "weight": 1
+          },
+          // Also boost first-seen if they don't have last-seen defined
+          {
+            "filter": {
+              "bool": {
+                "must_not": [
+                  {"exists": {"field": "last-seen"}}
+                ],
+                "must": [
+                  {
+                    "range": {
+                      "first-seen": {
+                        "from": "now-3M"
+                      }
+                    }
+                  }
+                ]
+              }
+            },
+            "weight": 1
+          },
+          {
+            "filter": {
+              "bool": {
+                "must_not": [
+                  {"exists": {"field": "last-seen"}}
+                ],
+                "must": [
+                  {
+                    "range": {
+                      "first-seen": {
+                        "from": "now-1M"
+                      }
+                    }
+                  }
+                ]
+              }
+            },
+            "weight": 1
+          },
+          {
+            "filter": {
+              "bool": {
+                "must_not": [
+                  {"exists": {"field": "last-seen"}}
+                ],
+                "must": [
+                  {
+                    "range": {
+                      "first-seen": {
+                        "from": "now-1d"
+                      }
+                    }
+                  }
+                ]
+              }
+            },
+            "weight": 1
           }
-      },
-      "_source": [
-        "metadata.title", "metadata.name", "metadata.description",
-        "references", "size", "last-seen", "first-seen"
-      ]
+        ]
+      }
+    },
+    "highlight": {
+      "order": "score",
+      "require_field_match": false,
+      "encoder": "html",
+      "fields": {
+        "*": {
+          "number_of_fragments": 1,
+          "fragment_size": 250
+        }
+      }
+    },
+    "_source": [
+      "metadata.title", "metadata.name", "metadata.description",
+      "references", "size", "last-seen", "first-seen"
+    ]
   };
+
+  // Decay functions are not working, somehow!
+  // {
+  //     // The relevancy of old documents is multiplied by at least one.
+  //     "weight": 1
+  // },
+  // {
+  //     // Seen today get a big boost
+  //     "weight": 5,
+  //     "gauss": {
+  //         "last-seen": {
+  //             "origin": "2017-04-07", // Change to current date
+  //             "scale": "31d",
+  //             "decay": 0.5
+  //         }
+  //     }
+  // },
+  // {
+  //     // Seen this month gets a smaller boost blished this year get a boost
+  //     "weight": 2,
+  //     "gauss": {
+  //         "last-seen": {
+  //             "origin": "2017-04-07", // Change to current date
+  //             "scale": "1M",
+  //             "decay": 0.5
+  //         }
+  //     }
+  // },
+  // {
+  //     // Seen in last 3 months get a smaller boost
+  //     "weight": 1,
+  //     "gauss": {
+  //         "last-seen": {
+  //             "origin": "2017-04-07", // Change to current date
+  //             "scale": "3M",
+  //             "decay": 0.5
+  //         }
+  //     }
+  // }
 
   return client.search({
     index: 'ipfs',
