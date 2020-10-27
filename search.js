119b3affe1b85f3aecff6474cf24bb9501e5dbee
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-10-27 11:22:08 +0000

    Fixus for c77461bfa445f8f7e706499db721e27f0f993b8e
    Must name *all* fields.

--- search.js
+++ search.js
@@ -33,125 +33,150 @@ function search(q, type, page, pageSize) {
 
   const body = {
     query: {
-      function_score: {
-        query: {
-          query_string: {
-            query: qWithoutType,
-            default_operator: 'AND',
-            fields: [
-              '"hash"^5',
-              '"metadata.isbn"^5',
-              'metadata.title^5',
-              'references.name^5',
-              'metadata.author^4',
-              'metadata.xmpDM:artist^4',
-              '"references.parent_hash"^3',
-              'metadata.xmpDM:composer^3',
-              'metadata.description^3',
-              'metadata.keywords^3',
-              'links.Name^3',
-              'metadata.xmpDM:album^2',
-              'metadata.publisher^2',
-              '"links.Hash"^2',
-              '*',
+      boosting: {
+        positive: {
+          function_score: {
+            query: {
+              query_string: {
+                query: qWithoutType,
+                default_operator: 'AND',
+                fields: [
+                  '"hash"^5',
+                  '"metadata.isbn"^5',
+                  'metadata.title^5',
+                  'metadata.name^5',
+                  'references.name^5',
+                  'metadata.author^4',
+                  'metadata.xmpDM:artist^4',
+                  '"references.parent_hash"^3',
+                  'metadata.xmpDM:composer^3',
+                  'metadata.description^3',
+                  'metadata.keywords^3',
+                  'links.Name^3',
+                  'metadata.xmpDM:album^2',
+                  'metadata.xmpDM:albumArtist^2',
+                  'metadata.publisher^2',
+                  'metadata.producer^2',
+                  '"links.Hash"^2',
+                  'metadata.Content-Type^2',
+                  'content',
+                  'fingerprint',
+                  // 'metadata.Author', // TO INDEX!!!
+                  // 'metadata.Keywords'
+                  // 'metadata.creator',
+                  // 'metadata.contributor'
+                  // 'metadata.subject',
+                  'urls',
+                ],
+              },
+            },
+            score_mode: 'sum',
+            boost_mode: 'multiply',
+            functions: [
+              {
+                weight: 1,
+              },
+              {
+                filter: {
+                  range: {
+                    'last-seen': {
+                      from: 'now-3M',
+                    },
+                  },
+                },
+                weight: 1,
+              },
+              {
+                filter: {
+                  range: {
+                    'last-seen': {
+                      from: 'now-1M',
+                    },
+                  },
+                },
+                weight: 1,
+              },
+              {
+                filter: {
+                  range: {
+                    'last-seen': {
+                      from: 'now-1d',
+                    },
+                  },
+                },
+                weight: 1,
+              },
+              // Also boost first-seen if they don't have last-seen defined
+              {
+                filter: {
+                  bool: {
+                    must_not: [
+                      { exists: { field: 'last-seen' } },
+                    ],
+                    must: [
+                      {
+                        range: {
+                          'first-seen': {
+                            from: 'now-3M',
+                          },
+                        },
+                      },
+                    ],
+                  },
+                },
+                weight: 1,
+              },
+              {
+                filter: {
+                  bool: {
+                    must_not: [
+                      { exists: { field: 'last-seen' } },
+                    ],
+                    must: [
+                      {
+                        range: {
+                          'first-seen': {
+                            from: 'now-1M',
+                          },
+                        },
+                      },
+                    ],
+                  },
+                },
+                weight: 1,
+              },
+              {
+                filter: {
+                  bool: {
+                    must_not: [
+                      { exists: { field: 'last-seen' } },
+                    ],
+                    must: [
+                      {
+                        range: {
+                          'first-seen': {
+                            from: 'now-1d',
+                          },
+                        },
+                      },
+                    ],
+                  },
+                },
+                weight: 1,
+              },
             ],
           },
         },
-        score_mode: 'sum',
-        boost_mode: 'multiply',
-        functions: [
-          {
-            weight: 1,
+        negative: {
+          bool: {
+            filter: [
+              { exists: { field: 'metadata.title' } },
+              { exists: { field: 'metadata.name' } },
+              { exists: { field: 'references.Name' } },
+            ],
           },
-          {
-            filter: {
-              range: {
-                'last-seen': {
-                  from: 'now-3M',
-                },
-              },
-            },
-            weight: 1,
-          },
-          {
-            filter: {
-              range: {
-                'last-seen': {
-                  from: 'now-1M',
-                },
-              },
-            },
-            weight: 1,
-          },
-          {
-            filter: {
-              range: {
-                'last-seen': {
-                  from: 'now-1d',
-                },
-              },
-            },
-            weight: 1,
-          },
-          // Also boost first-seen if they don't have last-seen defined
-          {
-            filter: {
-              bool: {
-                must_not: [
-                  { exists: { field: 'last-seen' } },
-                ],
-                must: [
-                  {
-                    range: {
-                      'first-seen': {
-                        from: 'now-3M',
-                      },
-                    },
-                  },
-                ],
-              },
-            },
-            weight: 1,
-          },
-          {
-            filter: {
-              bool: {
-                must_not: [
-                  { exists: { field: 'last-seen' } },
-                ],
-                must: [
-                  {
-                    range: {
-                      'first-seen': {
-                        from: 'now-1M',
-                      },
-                    },
-                  },
-                ],
-              },
-            },
-            weight: 1,
-          },
-          {
-            filter: {
-              bool: {
-                must_not: [
-                  { exists: { field: 'last-seen' } },
-                ],
-                must: [
-                  {
-                    range: {
-                      'first-seen': {
-                        from: 'now-1d',
-                      },
-                    },
-                  },
-                ],
-              },
-            },
-            weight: 1,
-          },
-        ],
+        },
+        negative_boost: 0.1,
       },
     },
     highlight: {
