776604cd7a12a906ddc8cdf0b1db0f54bb9f0308
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2023-02-06 13:09:43 +0000

    Slightly more sensible error handling.

--- server/server.js
+++ server/server.js
@@ -7,13 +7,19 @@ const cidValidator = require('./metadata/cid_validator');
 const app = express();
 const port = 9615;
 
-function error(res, code, err) {
-  console.error(`${code}: ${err}`);
-  console.trace(err);
+function error(req, res, code, err) {
+  console.error(`${req.url} ${code}: ${err}`);
 
   // Unwrap ES errors.
   if (typeof err === 'object' && 'body' in err && 'error' in err.body) {
-    console.trace(err.body.error);
+    err = err.body.error;
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    // Don't leak production errors
+    err = 'Internal Server Error'
+  } else {
+    console.trace(err);
   }
 
   res.status(code).json({ error: `${err}` }).end();
@@ -27,7 +33,7 @@ app.get('/search', (req, res, next) => {
   const pageSize = 15;
 
   if (!('q' in req.query)) {
-    error(res, 400, 'query argument missing');
+    error(req, res, 400, 'query argument missing');
   }
 
   let page = 0;
@@ -37,7 +43,7 @@ app.get('/search', (req, res, next) => {
 
     // For performance reasons, don't allow paging too far down
     if (page > maxPage) {
-      error(res, 400, 'paging not allowed beyond 100');
+      error(req, res, 400, 'paging not allowed beyond 100');
     }
   }
 
@@ -64,7 +70,7 @@ app.get('/metadata/:cid/', function (req, res, next) {
     let cid = req.params['cid']
 
     if (!cidValidator.Validate(cid)) {
-      error(res, 400, 'invalid cid');
+      error(req, res, 400, 'invalid cid');
     }
 
     getMetadata(cid).then((r) => {
@@ -73,16 +79,11 @@ app.get('/metadata/:cid/', function (req, res, next) {
 })
 
 app.use(function (err, req, res, next) {
-  if (process.env.NODE_ENV === 'production') {
-    // Don't leak details in production
-    error(res, 500, 'Internal Server Error');
-  }
-
   if (res.headersSent) {
     return next(err);
   }
 
-  error(res, 500, err);
+  error(req, res, 500, err);
 });
 
 app.listen(port, '127.0.0.1', () => console.log(`ipfs-search search API listening on port ${port}!`));
