1ddf7662ed5e7f3b46efe643b4cdc03e0c925ac4
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2023-03-20 16:38:14 +0000

    Mark parameters for not being in production.

--- packages/server/src/search/search.ts
+++ packages/server/src/search/search.ts
@@ -43,6 +43,8 @@ export class Searcher {
       max_concurrent_shard_requests: 24, // Defines the number of concurrent shard requests per node this search executes concurrently. This value should be used to limit the impact of the search on the cluster in order to limit the number of concurrent shard requests. Defaults to 5.
       pre_filter_shard_size: 2048,
       preference: "_local",
+
+      // Turn this on once we hit production.
       // timeout: '800ms', // Specifies the period of time to wait for a response from each shard. If no response is received before the timeout expires, the request fails and returns an error.
       // allow_partial_search_results: true, // If true, returns partial results if there are shard request timeouts or shard failures. If false, returns an error with no partial results.
       // ignore_unavailable: true, // If false, the request returns an error if it targets a missing or closed index.
